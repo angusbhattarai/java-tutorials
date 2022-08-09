75cf9e7919aa61646e6680db2f9903fe91bca66f
angusbhattarai <bhattaraiangus@gmail.com>
2022-08-09 23:03:30 +0545

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        String name="Angus Bhattarai";
+        int age=21;
+        System.out.println("My name is "+name+"."+"\nI am "+age+"years old.");
+    }
+}

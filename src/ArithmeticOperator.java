abb149f75c823ac6977cbf407f53793be11b3cc6
angusbhattarai <bhattaraiangus@gmail.com>
2022-08-11 15:38:47 +0545

    Improvements in arithmetic operator

--- src/ArithmeticOperator.java
+++ src/ArithmeticOperator.java
@@ -3,23 +3,37 @@ import java.util.Scanner;
 public class ArithmeticOperator {
 
     public static void main(String[] args) {
-        System.out.println("Enter a value of a :");
-        System.out.println("Enter a value of b :");
+        int a,b,add,sub,mult;
+        double div;
         Scanner java=new Scanner(System.in);
-        int a=java.nextInt();
-        int b =java.nextInt();
-        int c, d, e, g;
-        double f;
-        d = a + b;
-        c = a - b;
-        e = a * b;
-        f = (double) a / b;
-        g = a % b;
-        System.out.println("The Addition Output is " + d + "." + "" +
-                "\nThe Subtraction Output is " + c + "." +
-                "\nThe Multiplication Output is " + e + "." +
-                "\nThe Division Output is " + f + "." +
-                "\nThe percentage Output is " + g + ".");
+        System.out.println("Enter a value of a to add :");
+        a=java.nextInt();
+        System.out.println("Enter a value of b to add :");
+        b=java.nextInt();
+       add=a+b;
+        System.out.println("The Addition Output is " + add+ "." );
+       System.out.println("Enter the value of a to subtract:");
+       a=java.nextInt();
+       System.out.println("Enter the value of b to subtract :");
+       b=java.nextInt();
+       sub=a-b;
+        System.out.println("The Subtraction Output is "+sub+".");
+
+        System.out.println("Enter the value of a to multiply :");
+        a=java.nextInt();
+        System.out.println("Enter the value of b to multiply :");
+        b=java.nextInt();
+        mult=a*b;
+        System.out.println("The Multiplication Output is "+mult+".");
+        System.out.println("Enter the value of a to divide :");
+        a=java.nextInt();
+        System.out.println("Enter the value of b to divide :");
+        b=java.nextInt();
+        div=(double) a/b;
+        System.out.println("The divison Output is "+div+".");
+
+
+
     }
 
 }

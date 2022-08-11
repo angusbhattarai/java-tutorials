ad53cf67c54f0daf3e1e69565d125dc547e3815e
angusbhattarai <bhattaraiangus@gmail.com>
2022-08-11 16:16:27 +0545

    Improvements of Unary Operator

--- src/UnaryOperator.java
+++ src/UnaryOperator.java
@@ -2,12 +2,17 @@ public class UnaryOperator {
     public static void main(String args[]){
         int a=10;
         int b=13;
+        int c=11;
+        boolean d = false;
         //incremental
         a++; //a=a+1;
         //decremental
         b--; //b=b-1;
-        System.out.println(a);
-        System.out.println(b);
+        System.out.println("Incremental "+a);
+        System.out.println("Decremental "+b);
+        System.out.println(~c);
+        System.out.println(!d);
+
 
     }
 }

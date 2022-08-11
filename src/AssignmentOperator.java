d96c47ca7560fe3b6888777f7d4d9da3f9288552
angusbhattarai <bhattaraiangus@gmail.com>
2022-08-11 17:45:47 +0545

    Improvements of Assignment Operator

---
+++ src/AssignmentOperator.java
@@ -0,0 +1,28 @@
+public class AssignmentOperator {
+    public static void main(String[]args){
+        int a=8,b=2,c=9,d=3,f=5,g=1,h=10,i=12,j=13,k=20;
+        double e=4;
+        b+=2;
+        c-=4;
+        d*=5;
+        e/=5;
+        f%=8;
+        g&=4;
+        h|=12;
+        i^=5;
+        j>>=3;
+        k<<=5;
+        System.out.println("The value of a in equal is "+a+".");
+        System.out.println("The value of b in addition "+b+".");
+        System.out.println("The value of c in subtraction "+c+".");
+        System.out.println("The value of d in multiplication "+d+".");
+        System.out.println("The value of e in divison is "+e+".");
+        System.out.println("The value of f in percentage is "+f+".");
+        System.out.println("The value of g is "+g+".");
+        System.out.println("The value of h is "+h+".");
+        System.out.println("The value of i is "+i+".");
+        System.out.println("The value of j is "+j+".");
+        System.out.println("The value of k is "+k+".");
+
+    }
+}

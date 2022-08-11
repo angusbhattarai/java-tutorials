36dd49ea06d2e62f2bef3da62a949ac82239ca4e
angusbhattarai <bhattaraiangus@gmail.com>
2022-08-11 16:12:20 +0545

    Urnary Operator

---
+++ src/UnaryOperator.java
@@ -0,0 +1,13 @@
+public class UnaryOperator {
+    public static void main(String args[]){
+        int a=10;
+        int b=13;
+        //incremental
+        a++; //a=a+1;
+        //decremental
+        b--; //b=b-1;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}

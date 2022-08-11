833a9acb374c7b1926eef17f4781e9c9abac9473
angusbhattarai <bhattaraiangus@gmail.com>
2022-08-12 01:41:42 +0545

    Improvement of while loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[]args){
+        int i=0;
+        do {
+            System.out.println("Increment " + i);
+           i++;
+        }
+        while(i<5);
+    }
+}

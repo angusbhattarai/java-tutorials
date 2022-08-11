bfe96f03cbc5d4c18ddabc4ef824b16d9775e45b
angusbhattarai <bhattaraiangus@gmail.com>
2022-08-11 16:38:11 +0545

    Improvement of Shift Operator

---
+++ src/ShiftOperator.java
@@ -0,0 +1,9 @@
+public class ShiftOperator {
+    public static void main(String []args){
+        int a=5, b=6,c;
+        c=a<<b; //Left Shift Operator
+        System.out.println("The left shift operator is "+c);
+        c=a>>b; //Right Shift Operator
+        System.out.println("The Right shift operator is "+c);
+    }
+}
